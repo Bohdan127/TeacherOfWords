aeeb848ce73881bf2a0928efa97bdcd44235284d
Bohdan Benetekyi <benetskyybogdan@gmail.com>
2016-03-26 22:05:21 +0200

    Making default item template for Windows 8.1 app

---
+++ WordTracker/TeacherOfWords/DataModel/SampleDataItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TeacherOfWords.DataModel
+{
+    public class SampleDataItem
+    {
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public long UniqueId { get; set; }//todo should be private set here
+    }
+}
---
+++ WordTracker/TeacherOfWords/DataModel/SampleDataManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TeacherOfWords.DataModel
+{
+    public sealed class SampleDataManager
+    {
+        public static ObservableCollection<SampleDataItem> GetDeaultItemList()
+        {
+            ObservableCollection<SampleDataItem> resGroup = new ObservableCollection<SampleDataItem>();
+
+            var item = new SampleDataItem()
+            {
+                UniqueId = 1,
+                Title = "Some First Title",
+                Description = "New DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew DescriptionNew Description",
+                ImagePath = "Assets/SplashScreen.scale-100.png"
+            };
+
+            resGroup.Add(item);
+            resGroup.Add(item);
+            resGroup.Add(item);
+            resGroup.Add(item);
+            resGroup.Add(item);
+
+            return resGroup;
+        }
+    }
+
+    public class SampleDataItems : ObservableCollection<SampleDataItem>
+    {
+        public SampleDataItems()
+        {
+            foreach (var item in SampleDataManager.GetDeaultItemList())
+            {
+                Add(item);
+            }
+        }
+    }
+}
